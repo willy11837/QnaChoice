23be6a1a2f9a79465d1ccc5bcee1c4fc8540e7d8
Willy Kang <willykang6402@gmail.com>
2021-01-30 14:42:30 +0800

    Initial Commit

---
+++ QnaChoice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  QnaChoice
+//
+//  Created by Willy Kang on 2021/1/30.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
